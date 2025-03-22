aaec7c34faa8526fd31d3dc39b0a48f8857344f0
Brian <brian.kiprono8@student.moringaschool.com>
2025-03-22 08:38:15 +0300

    scope lab complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+//declare in global scope 
+var customerName = 'bob';
+//func to return customerName
+
+function upperCaseCustomerName(){
+    customerName = customerName.toLocaleUpperCase();
+}
+ var bestCustomer;
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+var overwriteBestCustomer;
+function overwriteBestCustomer (){
+    bestCustomer = "maybe bob"
+};
+
+const leastFavoriteCustomer = "Ian"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "titus"
+}
+changeLeastFavoriteCustomer();
